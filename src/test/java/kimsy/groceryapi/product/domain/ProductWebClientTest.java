7022ce6138ffe2d11928afad8aaa6108548366a2
SeolYoungKim <nasur4da@gmail.com>
2023-01-27 22:33:39 +0900

    test: ProductWebClient 테스트 코드 작성

---
+++ src/test/java/kimsy/groceryapi/product/domain/ProductWebClientTest.java
@@ -0,0 +1,92 @@
+package kimsy.groceryapi.product.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.List;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+class ProductWebClientTest {
+    public static MockWebServer mockWebServer;
+
+    @BeforeAll
+    static void beforeAll() throws IOException {
+        mockWebServer = new MockWebServer();
+        mockWebServer.start();
+    }
+
+    @AfterAll
+    static void afterAll() throws IOException {
+        mockWebServer.shutdown();
+    }
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private ProductWebClient productWebClient;
+
+    @BeforeEach
+    void setUp() {
+        final String baseUrl = String.format("http://localhost:%s", mockWebServer.getPort());
+
+        FruitWebClient fruitWebClient = new FruitWebClient(baseUrl);
+        VegetableWebClient vegetableWebClient = new VegetableWebClient(baseUrl);
+
+        productWebClient = new ProductWebClient(fruitWebClient, vegetableWebClient);
+    }
+
+    @DisplayName("품목(fruit/vegetable)이 전달되었을 때")
+    @Nested
+    class GetProducts {
+        private static final String ACCESS_TOKEN = "{ \"accessToken\" : \"token\" }";
+
+        @DisplayName("fruit이 전달된 경우 과일 목록을 반환한다.")
+        @Test
+        void fruitToProductNames() throws JsonProcessingException {
+            mockWebServer.enqueue(new MockResponse()
+                    .setBody(objectMapper.writeValueAsString(ACCESS_TOKEN))
+                    .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON));
+
+            final String[] expect = {"배", "토마토", "사과", "바나나"};
+            mockWebServer.enqueue(new MockResponse()
+                    .setBody(objectMapper.writeValueAsString(expect))
+                    .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON));
+
+            final List<String> actual = productWebClient.getProducts(
+                    ProductType.FRUIT.productTypeName());
+
+
+            assertThat(actual).hasSize(4);
+            assertThat(actual).contains(expect);
+        }
+
+        @DisplayName("vegetable이 전달된 경우 채소 목록을 반환한다.")
+        @Test
+        void vegetableToProductNames() throws JsonProcessingException {
+            mockWebServer.enqueue(new MockResponse()
+                    .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                    .addHeader(HttpHeaders.SET_COOKIE, "Authorization=token"));
+
+            final String[] expect = {"치커리", "토마토", "깻잎", "상추"};
+            mockWebServer.enqueue(new MockResponse()
+                    .setBody(objectMapper.writeValueAsString(expect))
+                    .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON));
+
+            final List<String> actual = productWebClient.getProducts(
+                    ProductType.VEGETABLE.productTypeName());
+
+
+            assertThat(actual).hasSize(4);
+            assertThat(actual).contains(expect);
+        }
+    }
+}
