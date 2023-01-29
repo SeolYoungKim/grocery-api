af4cab2f0618f14c00becdaa9245fb260fca4e01
SeolYoungKim <nasur4da@gmail.com>
2023-01-29 15:33:26 +0900

    feat: 외부 API의 500 InternalServerError 발생시 대응 로직 구현

--- src/main/java/kimsy/groceryapi/exception/presentation/WebClientExceptionController.java
+++ src/main/java/kimsy/groceryapi/exception/presentation/WebClientExceptionController.java
@@ -12,32 +12,37 @@ import org.springframework.web.reactive.function.client.WebClientResponseExcepti
 @Slf4j
 @ControllerAdvice
 public class WebClientExceptionController {
+    private static final String EXCEPTION_RESULT = "exceptionResult";
+
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(WebClientResponseException.NotFound.class)
     public String notFountHandler(Model model) {
         final ExceptionResult exceptionResult = new ExceptionResult(HttpStatus.NOT_FOUND.name(),
                 HttpStatus.NOT_FOUND.value(), "없는 품목입니다. 전체 목록의 품목명을 확인해 주세요.");
 
-        model.addAttribute("exceptionResult", exceptionResult);
+        model.addAttribute(EXCEPTION_RESULT, exceptionResult);
         return "/error/4xx";
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(WebClientResponseException.BadRequest.class)
     public String badRequestHandler(WebClientResponseException e, Model model) {
-        log.info("외부 API에서 예외 발생 : {}", e.getResponseBodyAs(String.class));
+        log.info("[API 400 ERROR] message={}", e.getMessage());
+        log.info("[API 400 ERROR] response body={}", e.getResponseBodyAs(String.class));
 
         final ExceptionResult exceptionResult = new ExceptionResult(HttpStatus.BAD_REQUEST.name(),
                 HttpStatus.BAD_REQUEST.value(), "잘못된 요청입니다.");
 
-        model.addAttribute("exceptionResult", exceptionResult);
+        model.addAttribute(EXCEPTION_RESULT, exceptionResult);
         return "/error/4xx";
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(WebClientResponseException.InternalServerError.class)
-    public String internalServerErrorHandler(WebClientResponseException e , Model model) {
-        e.getMessage();
-        return "";
+    public String internalServerErrorHandler(WebClientResponseException e) {
+        log.info("[API 500 ERROR] message={}", e.getMessage());
+        log.info("[API 500 ERROR] response body={}", e.getResponseBodyAs(String.class));
+
+        return "/error/5xx";
     }
 }
