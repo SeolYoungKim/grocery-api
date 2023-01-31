a65c71d54873d8c203350cfd740d4ac5aa32f5be
SeolYoungKim <nasur4da@gmail.com>
2023-01-31 12:07:56 +0900

    refactor: 예외 발생 시 로그를 남기도록 변경

--- src/main/java/kimsy/groceryapi/exception/presentation/BusinessExceptionController.java
+++ src/main/java/kimsy/groceryapi/exception/presentation/BusinessExceptionController.java
@@ -1,17 +1,21 @@
 package kimsy.groceryapi.exception.presentation;
 
 import kimsy.groceryapi.exception.presentation.dto.ExceptionResult;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@Slf4j
 @ControllerAdvice
 public class BusinessExceptionController {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class})
     public String illegalArgsHandler(RuntimeException e, Model model) {
+        log.info("[GroceryAPI 404 ERROR] message={}", e.getMessage());
+
         final ExceptionResult exceptionResult = new ExceptionResult(HttpStatus.BAD_REQUEST.name(),
                 HttpStatus.BAD_REQUEST.value(), e.getMessage());
 
